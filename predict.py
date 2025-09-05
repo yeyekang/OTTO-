516ea5744697bc057c229ffce5f3b33ab5f06b31
yeyekang <15249185085@163.com>
2025-09-05 20:50:19 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ from ranker import user_sample
 def submission(candidate_type):
     sub = pd.DataFrame()
     for t in candidate_type:
-        df = pd.read_parquet(f'/home/niejianfei/otto/LB/submission/sub_{t}.pqt')
+        df = pd.read_parquet(f'/home/kangqiman/otto/LB/submission/sub_{t}.pqt')
         df = df.loc[df.session_type.str.contains(t)]
         sub = sub.append(df)
     return sub
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     generate_submission('test', 'LB', candidate_type, user_sample(0.5), 'final_all_data')
 
     submission_final = submission(candidate_type)
-    submission_final.to_csv(f'/home/niejianfei/otto/LB/submission/submission_final.csv', index=False)
+    submission_final.to_csv(f'/home/kangqimanotto/LB/submission/submission_final.csv', index=False)
